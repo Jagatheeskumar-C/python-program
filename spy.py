f93d55dd7324a30c7d79bd0939f253a70c35c217
Jagatheeskumar-C <52210158+Jagatheeskumar-C@users.noreply.github.com>
2020-02-06 21:16:51 +0530

    Create spy.py

---
+++ spy.py
@@ -0,0 +1,17 @@
+a=int(input())
+b=str(a)
+s=0
+m=1
+for i in b:
+    s+=int(i)
+    m*=int(i)
+if s==m:
+    print("spy number")
+else:
+    print("not spy number")
+    
+    
+    
+    input=6
+    1+2+3=6
+    1*2*3=6
