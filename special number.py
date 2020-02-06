7e58ff11028ea6374ae41ead7988e4aaf9a61082
Jagatheeskumar-C <52210158+Jagatheeskumar-C@users.noreply.github.com>
2020-02-06 18:49:16 +0530

    Create special number.py

---
+++ special number.py
@@ -0,0 +1,20 @@
+a=int(input())
+b=str(a)
+sum=0
+v=1
+for i in b:
+    sum+=int(i)
+    v*=int(i)
+f=v+sum
+if a==f:
+    print("special numer")
+else:
+    print("not special number")
+    
+    
+    
+    input:59
+    5+9=14
+    5*9=45
+        =59
+        speecial number
